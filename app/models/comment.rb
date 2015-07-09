5d60138d5c6beeef13e03bfac9d8746053383130
“Nana <braverydream@gmail.com>
2015-07-09 11:35:59 +0800

    add user_id to commentDB

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ActiveRecord::Base
   belongs_to :idea
+  belongs_to :owner, class_name: "User", foreign_key: :user_id
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150707105816) do
+ActiveRecord::Schema.define(version: 20150709032358) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "user_name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150707105816) do
     t.integer  "idea_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "ideas", force: :cascade do |t|
